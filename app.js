e8db88dd174208d5bb3e0bca059b6da04ab09d6f
Maskwelldev <102791393+Maskwelldev@users.noreply.github.com>
2023-01-23 12:00:54 +0100

    okokook

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 
 
 // On récupère le paquet express
+const checkAuth = require('./helpers/jwt');
 const express = require('express')
 const morgan = require('morgan')
 const {success,getSwagger} = require('./helper')
 const usersRouter  = require('./routes/users.router')
 const rolesRouter  = require('./routes/roles.router')
-const swaggerUI = require('swagger-ui-express')
+const swaggerUI = require('swagger-ui-express');
+const myJwt = require('./helpers/jwt');
+const jwt = require('jsonwebtoken');
 
 
 //On créé une instance d'une application express (c'est notre serveur)
 const app = express()
-
-const checkAuth = require('./helpers/jwt');
-router.get('/', checkAuth, (req, res, next) => {});
 
 
 app.use(express.json())
@@ -31,12 +31,20 @@ const port = 3000
 
 //Premier point de terminaison. Dans un premier temps, le première argument est la route, le deuxième paramètre est une fonction qui recoit une requête et qui renvoie une réponse (req et res).
 // on utilise la méthode send de la réponse pour renvoyer un message
-app.get('/', (req,res) => {
-    
+app.get('/', checkAuth, (req,res) => {
     const message = "Bienvenue sur notre API"
     const data =  'coucou'
-    res.json(success(message,data))
+    res.json(success(message,data));
 })
+
+app.get('/getToken', (req, res) => {
+  const payload = {id: 1, email: 'pif@fmail.com'};
+  const secret = 'monsupersecret';
+  const options = { expiresIn: '2min' };
+  const token = jwt.sign(payload, secret, options);
+  console.log(token);
+  res.status(200).json({message: 'Token generated', token: token});
+});
 
 
 
