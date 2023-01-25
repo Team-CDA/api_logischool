62e1ab6168023de893ac54c26c083b22661724a5
Maskwelldev <102791393+Maskwelldev@users.noreply.github.com>
2023-01-25 14:15:18 +0100

    fezfezgezgez

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const port = 3000
 // on utilise la méthode send de la réponse pour renvoyer un message
 app.get('/', (req,res) => {
     const message = "Bienvenue sur notre API"
-    const data =  'MDR'
+    const data =  'lolilol'
     res.json(success(message,data));
 })
 
