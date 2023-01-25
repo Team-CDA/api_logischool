a535700b21100e2b9502911700c9d38714bb3cdd
bibou <bibobens@hotmail.com>
2023-01-25 16:28:37 +0100

    Creation classes routes

---
+++ routes/classes.router.js
@@ -0,0 +1,108 @@
+const { Router } = require ("express")
+const ClassesController = require("../controllers/Classes-controller")
+const router = Router()
+
+/** 
+ * @swagger
+ * 
+ * /Classes:
+ *    get:
+ *      tags: [Classes]
+ *      summary: Récupère la liste de toutes les Classes
+ *      description: Récupère la liste de toutes les Classes
+ *      responses: 
+ *         200:
+ *            description: Success
+ * 
+ */
+router.get('/', ClassesController.getAll)
+
+/** 
+ * @swagger
+ * 
+ * /Classes:
+ *    get:
+ *      tags: [Classes]
+ *      summary: Récupère une Classe par son id
+ *      description: Récupère une Classe par son id
+ *      responses: 
+ *         200:
+ *            description: Success
+ * 
+ */
+router.get('/:id', ClassesController.getOneById)
+
+/** 
+ * @swagger
+ * 
+ * /Classes/create:
+ *    post:
+ *      tags: [Classes]
+ *      requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *              schema:
+ *                 $ref: '#/components/schemas/Classe'
+ *      summary: Permet d'ajouter une Classe
+ *      description: Permet d'ajouter une Classe.
+ *      responses: 
+ *         200:
+ *            description: Classe successfully created
+ */
+router.post('/create', ClassesController.createOne)
+
+
+/** 
+ * @swagger
+ * 
+ * /Classes/update/{id}:
+ *    patch:
+ *      tags: [Classes]
+ *      summary: Permet La modification d'une Classe
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *            schema:
+ *               $ref: '#/components/schemas/Classe'
+ *    description: Grâce à l'id d'une Classe vous pouvez la modifier.
+ *    responses:
+ *         200:
+ *          description: alert types successfully updated
+ *         
+ */
+router.patch('/update/:id', ClassesController.updateOneById)
+
+/** 
+ * @swagger
+ * 
+ * /Classes/delete/{id}:
+ *    delete:
+ *      tags: [Classes]
+ *      summary: Permet La suppressin d'une Classe
+ *      description: Passer l'id de la Classe que vous souhaitez supprimer en url et le tour est joué.
+ *      responses:
+ *         200:
+ *          description: class successfully deleted
+ *         
+ */
+router.delete('/delete/:id', ClassesController.deleteOneById)
+
+
+/** 
+ * @swagger
+ * 
+ * /Classes/deleteAll:
+ *    delete:
+ *      tags: [Classes]
+ *      summary: Permet La suppression de toutes les Classes (truncate)
+ *      description: Supprime la table en faisant un truncate, donc les ID repartent à 1
+ *      responses:
+ *         200:
+ *          description: Classes successfully deleted
+ *         
+ */
+router.delete('/deleteAll', ClassesController.deleteAll)
+
+module.exports = router
