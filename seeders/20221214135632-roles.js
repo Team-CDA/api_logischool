8fee30a6264787140a31ef6f2ae0972157ac3d58
NADIR BEN SALAH <95688259+Nadir-Bensalah@users.noreply.github.com>
2023-01-27 08:44:27 +0100

    youpii

--- seeders/20221214135632-roles.js
+++ seeders/20221214135632-roles.js
@@ -16,5 +16,6 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     // await queryInterface.sequelize.query('TRUNCATE TABLE roles');
   }  
-  
 };
+
+
--- seeders/20230123135549-genders.js
+++ seeders/20230123135549-genders.js
@@ -11,10 +11,9 @@ module.exports = {
       ], {});
     
   },
-
   async down (queryInterface, Sequelize) {
     await queryInterface.sequelize.query('TRUNCATE TABLE genders');
   }
-  
-  
 };
+
+
