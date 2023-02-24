c34e069a0f7cca16b091ae5c0d9ce5434bf6c0b3
Pisnoca Nhem <pisnoca_nhem@hotmail.fr>
2023-02-24 14:51:05 +0100

    seeders users subject good

---
+++ seeders/20230224144200-users_subjects.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+        "users_subjects",
+        [
+        {
+            id_user: 1,
+            id_subject: 1,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id_user: 2,
+            id_subject: 2,
+            createdAt: new Date(),
+            updatedAt: new Date()      
+        },
+        {
+            id_user: 3,
+            id_subject: 3,
+            createdAt: new Date(),
+            updatedAt: new Date()      
+        },
+        {
+            id_user: 4,
+            id_subject: 4,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id_user: 5,
+            id_subject: 5,
+            createdAt: new Date(),
+            updatedAt: new Date() 
+        },
+        ],
+        {}
+    );
+    },
+
+    async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query('TRUNCATE TABLE users_subjects');
+    },
+};
