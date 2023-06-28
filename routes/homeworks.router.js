97a19884e1b0b3ff6738a2de227083116cc0f276
Etienne-Devillers <devillers.etienne80@gmail.com>
2023-06-28 09:19:10 +0200

    début homework

--- routes/homeworks.router.js
+++ routes/homeworks.router.js
@@ -1,24 +1,23 @@
-const { Router } = require ("express")
-const router = Router()
-const homeworksController = require ("../controllers/homeworks-controller")
-const multerMultipleFiles = require('../helpers/multer-config')
+const { Router } = require("express");
+const router = Router();
+const homeworksController = require("../controllers/homeworks-controller");
+const multerMultipleFiles = require("../helpers/multer-config");
 
-module.exports = router
-
+module.exports = router;
 
 /**
  * @swagger
- *  components: 
+ *  components:
  *    schemas:
  *       homeworks:
  *          type: object
- *          required: 
+ *          required:
  *              - role
- *          properties: 
+ *          properties:
  *             id:
  *               type: integer
  *               description: homework's id
- *             homework_image: 
+ *             homework_image:
  *               type: string
  *               description: exercice file name
  *             correction_image:
@@ -32,20 +31,20 @@ module.exports = router
  *               description: user's id (the teacher that owns homeworks)
  *             id_subject:
  *               type: integer
- *               description: subject's id          
- *          example: 
+ *               description: subject's id
+ *          example:
  *             id: 1
- *             name: A  
+ *             name: A
  *             homework_image: 'exercice-1.png'
  *             correction_image: 'correction-1.png'
  *             id_user: 1
  *             id_subject: 1
- * 
-*/
+ *
+ */
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/classes/daterange:
  *    get:
  *      tags: [Homeworks]
@@ -60,15 +59,19 @@ module.exports = router
  *            type: dateTime
  *      summary: get homeworks_classes for a daterange
  *      description: by specifying two dates, you can retrieve all elements contained inbetween the daterange provided
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks_classes successfully retrieved
  */
-router.get('/classes/daterange', multerMultipleFiles, homeworksController.getByDateRangeHC)
+router.get(
+  "/classes/daterange",
+  multerMultipleFiles,
+  homeworksController.getByDateRangeHC
+);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/filter?:
  *    get:
  *      tags: [Homeworks]
@@ -80,10 +83,10 @@ router.get('/classes/daterange', multerMultipleFiles, homeworksController.getByD
  *       - in: query
  *         name: id_subject
  *         type: integer
- *         description: the subject id 
+ *         description: the subject id
  *      summary: get all homeworks
  *      description: Get list of all homeworks,if provided, you can filter the resuls with id_teacher or id_subject
- *      responses: 
+ *      responses:
  *         200:
  *            description: homework(s) successfully retrieved
  *            content:
@@ -95,17 +98,17 @@ router.get('/classes/daterange', multerMultipleFiles, homeworksController.getByD
  *         404:
  *            description: no homeworks found
  */
-router.get('/filter', multerMultipleFiles, homeworksController.getWithFilter)
+router.get("/filter", multerMultipleFiles, homeworksController.getWithFilter);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/:
  *    get:
  *      tags: [Homeworks]
  *      summary: get all homeworks
  *      description: Get list of all homeworks
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks successfully retrieved
  *            content:
@@ -117,11 +120,11 @@ router.get('/filter', multerMultipleFiles, homeworksController.getWithFilter)
  *         404:
  *            description: no homeworks found
  */
-router.get('/', multerMultipleFiles, homeworksController.getAll)
+router.get("/", multerMultipleFiles, homeworksController.getAll);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/{id}:
  *    get:
  *      tags: [Homeworks]
@@ -133,7 +136,7 @@ router.get('/', multerMultipleFiles, homeworksController.getAll)
  *          schema:
  *            type: integer
  *          required: true
- *      responses: 
+ *      responses:
  *         200:
  *            description: homework successfully retrieved
  *            content:
@@ -145,11 +148,11 @@ router.get('/', multerMultipleFiles, homeworksController.getAll)
  *         404:
  *            description: this specific homework was not found
  */
-router.get('/:id', multerMultipleFiles, homeworksController.getOneById)
+router.get("/:id", multerMultipleFiles, homeworksController.getOneById);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/create:
  *    post:
  *      tags: [Homeworks]
@@ -161,15 +164,17 @@ router.get('/:id', multerMultipleFiles, homeworksController.getOneById)
  *                 $ref: '#/components/schemas/homeworks'
  *      summary: create homework
  *      description: create homework linked to a specfic user.
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks successfully created
  */
-router.post('/', multerMultipleFiles, homeworksController.create)
+router.post("/", multerMultipleFiles, homeworksController.create);
 
-/** 
+router.put("/link", multerMultipleFiles, homeworksController.linkTwoFiles);
+
+/**
  * @swagger
- * 
+ *
  * /homeworks/update/{id}:
  *    put:
  *      tags: [Homeworks]
@@ -181,16 +186,15 @@ router.post('/', multerMultipleFiles, homeworksController.create)
  *                 $ref: '#/components/schemas/homeworks'
  *      summary: update homework
  *      description: based on a id, you can update an homework, or files linked to it.
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks successfully updated
  */
-router.put('/:id', multerMultipleFiles, homeworksController.update)
+router.put("/:id", multerMultipleFiles, homeworksController.update);
 
-
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/delete/{id}:
  *    delete:
  *      tags: [Homeworks]
@@ -202,20 +206,19 @@ router.put('/:id', multerMultipleFiles, homeworksController.update)
  *          required: true
  *          description: the homework Id you want to delete
  *      summary: By using an id you can delete a homework and all linked files
- *      description: 
- *      responses: 
+ *      description:
+ *      responses:
  *         200:
  *            description: homework successfully deleted
  *         404:
  *            description: the homeworks table was not found
- *         
+ *
  */
-router.delete('/:id', multerMultipleFiles, homeworksController.deleteOneById)
+router.delete("/:id", multerMultipleFiles, homeworksController.deleteOneById);
 
-
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/{id}/{file}:
  *    get:
  *      tags: [Homeworks]
@@ -233,16 +236,19 @@ router.delete('/:id', multerMultipleFiles, homeworksController.deleteOneById)
  *          required: true
  *          description: the file type you want to DL =>  exercice/correction
  *      summary: By using an id and a file type you can download the file
- *      description: 
- *      responses: 
+ *      description:
+ *      responses:
  *         200:
  *            description: homework successfully deleted
  *         404:
  *            description: the homeworks table was not found
- *         
+ *
  */
-router.get('/:id/:file', multerMultipleFiles, homeworksController.getOneFileById)
-
+router.get(
+  "/:id/:file",
+  multerMultipleFiles,
+  homeworksController.getOneFileById
+);
 
 // **************************************************
 // *************Partie homeworks_classes*************
@@ -250,13 +256,13 @@ router.get('/:id/:file', multerMultipleFiles, homeworksController.getOneFileById
 
 /**
  * @swagger
- *  components: 
+ *  components:
  *    schemas:
  *       homeworks_classes:
  *          type: object
- *          required: 
+ *          required:
  *              - role
- *          properties: 
+ *          properties:
  *             id:
  *               type: integer
  *               description: association's id
@@ -271,18 +277,18 @@ router.get('/:id/:file', multerMultipleFiles, homeworksController.getOneFileById
  *               description: plannified date for a class and a homework
  *             correction_date:
  *               type: integer
- *               description: date to make correction visible for everyone(can be used as a boolean)       
- *          example: 
- *             id_class: 1  
+ *               description: date to make correction visible for everyone(can be used as a boolean)
+ *          example:
+ *             id_class: 1
  *             id_homework: 1
  *             plannified_date: 2022-01-01 12:00:00
  *             correction_date: 2022-01-05 12:00:00
- * 
-*/
+ *
+ */
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/classes/{id}:
  *    get:
  *      tags: [Homeworks]
@@ -294,15 +300,19 @@ router.get('/:id/:file', multerMultipleFiles, homeworksController.getOneFileById
  *          required: true
  *      summary: get homeworks_classes
  *      description: get homeworks linked to a specfic user.
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks_classes successfully retrieved
  */
-router.get('/classes/:id', multerMultipleFiles, homeworksController.getOneByIdHC)
+router.get(
+  "/classes/:id",
+  multerMultipleFiles,
+  homeworksController.getOneByIdHC
+);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/classes/create:
  *    post:
  *      tags: [Homeworks]
@@ -314,15 +324,19 @@ router.get('/classes/:id', multerMultipleFiles, homeworksController.getOneByIdHC
  *                 $ref: '#/components/schemas/homeworks_classes'
  *      summary: create homework
  *      description: create homework associated to a specific class.
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks_classes successfully created
  */
-router.post('/classes/create', multerMultipleFiles, homeworksController.createHC)
+router.post(
+  "/classes/create",
+  multerMultipleFiles,
+  homeworksController.createHC
+);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/classes/update/{id}:
  *    put:
  *      tags: [Homeworks]
@@ -340,15 +354,19 @@ router.post('/classes/create', multerMultipleFiles, homeworksController.createHC
  *                 $ref: '#/components/schemas/homeworks_classes'
  *      summary: update homeworks_classes
  *      description: update homework linked to a specfic user.
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks_classes successfully updatted
  */
-router.put('/classes/update/:id', multerMultipleFiles, homeworksController.updateHC)
+router.put(
+  "/classes/update/:id",
+  multerMultipleFiles,
+  homeworksController.updateHC
+);
 
-/** 
+/**
  * @swagger
- * 
+ *
  * /homeworks/classes/{id}:
  *    delete:
  *      tags: [Homeworks]
@@ -360,11 +378,12 @@ router.put('/classes/update/:id', multerMultipleFiles, homeworksController.updat
  *          required: true
  *      summary: delete homeworks_classes
  *      description: delete homeworks linked to a specfic classes.
- *      responses: 
+ *      responses:
  *         200:
  *            description: homeworks_classes successfully deleted
  */
-router.delete('/classes/:id', multerMultipleFiles, homeworksController.deleteOneByIdHC)
-
-
-
+router.delete(
+  "/classes/:id",
+  multerMultipleFiles,
+  homeworksController.deleteOneByIdHC
+);
