2797a5a5bffff3628fcc413b3843307f0d710d52
nbsdecayeux <117639922+Nadirbens@users.noreply.github.com>
2023-10-08 09:52:22 +0200

    ok

--- config/config.js
+++ config/config.js
@@ -1,31 +1,24 @@
-require('dotenv').config();
 
-module.exports = {
+require('dotenv').config();
+module.exports={
   "development": {
     "username": process.env.SQL_USER,
     "password": process.env.SQL_PASSWORD,
-    "database": process.env.SQL_NAME,
-    "host": process.env.SQL_URL,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": process.env.SQL_USER,
-    "password": process.env.SQL_PASSWORD,
-    "database": process.env.SQL_NAME,
-    "host": process.env.SQL_URL,
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": process.env.SQL_USER,
-    "password": process.env.SQL_PASSWORD,
-    "database": process.env.SQL_NAME,
+    "database": "logischool",
     "host": process.env.SQL_URL,
     "dialect": "mysql"
   },
   "local": {
     "username": process.env.SQL_USER,
     "password": process.env.SQL_PASSWORD,
-    "database": process.env.SQL_NAME,
+    "database": "logischool",
+    "host": process.env.SQL_URL,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.SQL_USER_PROD,
+    "password": process.env.SQL_PASSWORD_PROD,
+    "database": "logischool",
     "host": process.env.SQL_URL,
     "dialect": "mysql"
   }
